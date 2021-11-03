5855a7875456b5f68dc54b4d0d3f42aa42a28c02
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-11-03 14:22:50 +0300

    Done Cards page

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,49 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router";
+import axios from "axios";
 import "./style.css";
 
 const Card = () => {
+
+  const [arr, setArr]= useState([]);
+
+  useEffect ( ()=>{ getPo()},[]);
+   
+
+    const getPo =async ()=>{
+      const response = await axios.get("https://api.pokemontcg.io/v2/cards?pageSize=10");
+      console.log(response.data.data);
+      setArr(response.data.data)
+    }
+     
   const id = useParams().id;
-  
-  const [arr] = useState([
-    {
-      id: 0,
-      name: "Heart",
-      img: "./heart.jpg",
-      disc: "Heart Card",
-    },
-    {
-      id: 1,
-      name: "Club",
-      img: "./club.png",
-      disc: "Club Card",
-    },
-    {
-      id: 2,
-      name: "Spade",
-      img: "./spade.png",
-      disc: "Spade Card",
-    },
-    {
-      id: 3,
-      name: "Diamond",
-      img: "./diamond.png",
-      disc: "Diamond Card",
-    },
-  ]);
+
 
   const cardElem= arr.find(elem => (
     (Number(elem.id) === Number(id))
   ))
+  console.log(cardElem);
 
 
 
   return (
     <div>  
-      <img src={require(`${cardElem.img}`).default} alt={`card ${cardElem.name}`} />
+      {/* <img src={cardElem.images.large}/>
       <h2> {cardElem.name} </h2>
-      <p>{cardElem.disc}</p>
+      <p>{cardElem.disc}</p> */}
     </div>
   );
 };
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,84 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-import { useHistory } from "react-router";
-import {AiOutlineLike} from "react-icons/ai"
+// import { useHistory } from "react-router";
+import axios from "axios";
 import "./style.css";
 
 
 const Cards = () => {
-  const [arr,setArr] = useState([
-    {
-      id: "0",
-      name: "Heart",
-      img: "./heart.jpg",
-      disc: "Heart Card",
-      isLike: false,
-    },
-    {
-      id: "1",
-      name: "Club",
-      img: "./club.png",
-      disc: "Club Card",
-      isLike: true,
-    },
-    {
-      id: "2",
-      name: "Spade",
-      img: "./spade.png",
-      disc: "Spade Card",
-      isLike: false,
-    },
-    {
-      id: "3",
-      name: "Diamond",
-      img: "./diamond.png",
-      disc: "Diamond Card",
-      isLike: false,
-    },
-  ]);
-  localStorage.setItem("arr", JSON.stringify(arr));
- 
-  const history = useHistory()
 
-  const gitPath=(id)=>{
-      history.push(`/Card/${id}`);
+  const [arr, setArr]= useState([]);
+
+useEffect ( ()=>{ getPo()},[]);
+ 
+  // const history = useHistory()
+  // const gitPath=(id)=>{
+  //     history.push(`/Card/${id}`);
     
-  } 
-  const searchPath=(e)=>{
-    //   console.log(e.target.search.value);
-    history.push(`/Search/${e.target.search.value}`);
+  // } 
+  const getPo =async ()=>{
+    const response = await axios.get("https://api.pokemontcg.io/v2/cards?pageSize=10");
+    console.log(response.data.data);
+    setArr(response.data.data)
   }
-
-const likeFunc=(e,i)=>{
- 
-//  elem.isLike = !elem.isLike
- 
-    console.log('index: ' + i);
-    console.log(arr[i].isLike);
-    let newArr = [...arr]; // copying the old datas array
-    newArr[i].isLike = !arr[i].isLike; // replace e.target.value with whatever you want to change it to
-  
-    setArr(newArr);
-    
    
-  }
 
 
-  
 
   return (
     <div >
-   <form onSubmit={searchPath}>
+   <form>
             <input  type="text"  name="search" placeholder="Search... "/>
             <button>Search</button>
         </form>
         <div className="cards">
       {arr.map((elem,i) => (
         <div key={elem.id} className="card" >
-          <img src={require(`${elem.img}`).default} alt={`card ${elem.name}`}   onClick={()=>{gitPath(elem.id)}}/>
+          <img src={elem.images.large} alt={`card ${elem.name}`}/> 
+          {/* onClick={()=>{gitPath(elem.id)}} */}
           <h2> {elem.name}  </h2>
-    
-          <AiOutlineLike id="icon"  onClick={(e)=>{likeFunc(e,i)}} className={elem.isLike ? `like` : `unlike`}/>
           
           
         </div>
